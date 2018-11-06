fab51c9afec312399777c4cd6082aea3db023d06
Brittanie Pham <brittaniepham@gmail.com>
2018-11-05 20:25:59 -0700

    remove comments

--- DemoWrapperClass.java
+++ DemoWrapperClass.java
@@ -3,26 +3,6 @@ package lab13_wrapper_class_GUI.labWrapperClassGui;
 public class DemoWrapperClass {
 
 	String minMax() {
-//		System.out.println("Byte: ");
-//		System.out.println("Min: " + Byte.MIN_VALUE);
-//		System.out.println("Max: " + Byte.MAX_VALUE);
-//		System.out.println();
-//		
-//		System.out.println("Short: ");
-//		System.out.println("Min: " + Short.MIN_VALUE);
-//		System.out.println("Max: " + Short.MAX_VALUE);
-//		System.out.println();
-//		
-//		System.out.println("Integer: ");
-//		System.out.println("Min: " + Integer.MIN_VALUE);
-//		System.out.println("Max: " + Integer.MAX_VALUE);
-//		System.out.println();
-//		
-//		System.out.println("Long: ");
-//		System.out.println("Min: " + Long.MIN_VALUE);
-//		System.out.println("Max: " + Long.MAX_VALUE);
-//		System.out.println();
-		
 		return 	(
 					"Byte: \n" +
 					"Min: " + Byte.MIN_VALUE +
@@ -43,10 +23,6 @@ public class DemoWrapperClass {
 	}
 	
 	String toBinary(int number) {
-//		System.out.println("Binary: " + Integer.toBinaryString(number));
-//		System.out.println("Octal: " + Integer.toOctalString(number));
-//		System.out.println("Hex: " + Integer.toHexString(number));
-		
 		return 	(
 					"Binary: " + Integer.toBinaryString(number) +
 					"\nOctal: " + Integer.toOctalString(number) + 
@@ -56,10 +32,6 @@ public class DemoWrapperClass {
 	}
 	
 	String charProperties(char c) {
-//		System.out.println("White space: " + Character.isWhitespace(c));
-//		System.out.println("Digit: " + Character.isDigit(c));
-//		System.out.println("Letter: " + Character.isLetter(c));
-		
 		return 	(
 					"White space: " + Character.isWhitespace(c) +
 					"\nDigit: " + Character.isDigit(c) +
